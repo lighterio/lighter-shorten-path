830ad135fbf21250764986c8fa171f793a3687ab
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:24:17 -0700

    Initial commit

---
+++ test/shorten-path-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var shorten-path = require(__dirname + '/lighter-shorten-path')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/shorten-path-test.js
@@ -0,0 +1,26 @@
+var shorten-path = require('../lighter-shorten-path')
+var is = global.is || require('exam/lib/is')
+
+describe('shorten-path', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
